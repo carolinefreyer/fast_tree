86a66f4baabad39dc95253092bad2ebe53277e2c
Pia Keukeleire <piakeuk@hotmail.com>
2021-01-19 15:16:56 +0100

    path to data was wrong

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import fastTree as ft
 # Initialises all the global variables and calls neighbourhood join
 def run():
     tree = ft.FastTree()
-    tree.initialize_sequences('data/fasttree-reallysmall.aln')
+    tree.initialize_sequences('../data/fasttree-reallysmall.aln')
     tree.initialize_top_hits()
     while(len(tree.ACTIVE)>1):
         tree.neighborJoin()
