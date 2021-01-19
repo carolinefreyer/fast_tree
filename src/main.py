d99cf2510d11bf4f0ed519c0a407328d4d477730
Pia Keukeleire <piakeuk@hotmail.com>
2021-01-19 15:53:31 +0100

    update readme

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 import fastTree as ft
 
+
 # Initialises all the global variables and calls neighbourhood join
 def run():
+    out = open('../data/fasttree-newick-output.txt', 'w')
     tree = ft.FastTree()
     tree.initialize_sequences('../data/fasttree-reallysmall.aln')
     tree.initialize_top_hits()
-    while(len(tree.ACTIVE)>1):
+    while (len(tree.ACTIVE) > 1):
         tree.neighborJoin()
     tree.computeBranchLenghts()
-    print(tree.newickFormat(tree.ACTIVE[0],""))
+    print(tree.newickFormat(tree.ACTIVE[0], ""))
     tree.nearestNeighbourInterchange()
     tree.computeBranchLenghts()
-    newickFormat = tree.newickFormat(tree.ACTIVE[0],"")
+    newickFormat = tree.newickFormat(tree.ACTIVE[0], "")
     newickFormat += ";"
+    out.write(newickFormat)
     print(newickFormat)
+
 
 run()
